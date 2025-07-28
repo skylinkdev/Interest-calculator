832e4d5c76ad9da1713d4216ee176af3dcc3ab72
chengqishun <chengqishun@gmail.com>
2025-07-28 10:06:19 +0800

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/interest-calculator/", // 注意：这里是 GitHub Pages 上的子路径
+  base: "/Interest-calculator/", // 注意首字母大写
   plugins: [react()],
   server: {
     port: 3000,
